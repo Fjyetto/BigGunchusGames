1392405dd4a8566c8273a2c10139a3c9dc43cc5c
Papatomique <henry.rochet@orange.fr>
2023-06-03 18:59:11 +0200

    RESPONSIVE DESIGN YEAHH
    WE GOT MOBILE WORKING UP IN HERE

---
+++ common.js
@@ -0,0 +1,13 @@
+if(window.innerHeight > window.innerWidth){
+	let elements = document.querySelectorAll('.top');
+		elements.forEach((element) => {
+		element.classList.remove('top');
+		element.classList.add('topmob');
+	});
+	
+	elements = document.querySelectorAll('.box');
+		elements.forEach((element) => {
+		element.classList.remove('box');
+		element.classList.add('boxmob');
+	});
+}
